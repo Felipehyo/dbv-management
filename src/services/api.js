2c88f1203aab20d433addbfb15ece4f66cb06c54
Felipe Pinheiro Dantas <felipehyo@gmail.com>
2023-02-28 21:28:32 -0300

    Adjust

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://dbv-financeiro.herokuapp.com/pathfinders/'
-    baseURL: 'http://localhost:8080/pathfinders/'
+    baseURL: 'https://dbv-financeiro.herokuapp.com/pathfinders/'
+    //baseURL: 'http://localhost:8080/pathfinders/'
 })
 
 export default api;
