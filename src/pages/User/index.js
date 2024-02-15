443fb8087ae7e63c25f1ee906a9f0feddd5562eb
Felipe Pinheiro Dantas <felipehyo@gmail.com>
2024-02-15 15:13:35 -0300

    add search by user type

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -8,6 +8,7 @@ import Nav from '../../components/Nav';
 const User = () => {
 
     const [ userList, setUserList ] = useState([]);
+    const [ userType, setUserType ] = useState('PATHFINDER');
 
     const clubId = sessionStorage.getItem("clubId");
     const navigate = useNavigate();
@@ -25,8 +26,15 @@ const User = () => {
         navigate("/user/edit");
     }
 
+    function getUsersByType(inputUserType) {
+        api.get('/user/club/' + clubId + '/type?userType=' + inputUserType).then(response => {
+            setUserType(inputUserType);
+            setUserList(response.data);
+        });
+    }
+
     useEffect(() => {
-        api.get('/user/club/' + clubId + '?eventualUser=true&onlyActives=false').then(response => {
+        api.get('/user/club/' + clubId + '/type?userType=PATHFINDER').then(response => {
             setUserList(response.data);
         });
     }, [clubId]);
@@ -36,22 +44,36 @@ const User = () => {
             <div className="container-user">
                 <div className="sub-container-user">
                     <Nav handleBack={handleBack}/>
-                    <img className="logo" src={"https://cdn-icons-png.flaticon.com/512/5259/5259256.png"} alt=""/>
+                    <img className="logo" src={"https://cdn-icons-png.flaticon.com/512/1165/1165725.png"} alt=""/>
                     <h1 className="nav-title">Usuários Cadastrados</h1>
+                    <div className='bts'>
+                        <button id={'pathfinder'}
+                            className={'bt-pathfinder' + (userType === 'PATHFINDER' ? ' selected' : '')}
+                            onClick={() => getUsersByType('PATHFINDER')}>Desbravadores</button>
+                        <button id={'direction'}
+                            className={'bt-direction' + (userType === 'DIRECTION' ? ' selected' : '')}
+                            onClick={() => getUsersByType('DIRECTION')}>Diretoria</button>
+                        <button id={'eventual'}
+                            className={'bt-eventual' + (userType === 'EVENTUAL' ? ' selected' : '')}
+                            onClick={() => getUsersByType('EVENTUAL')}>Eventuais</button>
+                    </div>
                     <section className="section">
                     {
                         userList.sort((a, b) => a.name - b.name).map((user, id) => (
                             <div className="card" key={id} onClick={() => handleEditUser(user.id)}>
+                                <div className={'status' + (user.active ? ' active' : ' inactive')}/>
                                 <div className="image">
                                     <img src={user.gender === 'MALE' ? "https://cdn-icons-png.flaticon.com/512/2922/2922506.png" : "https://cdn-icons-png.flaticon.com/512/2922/2922566.png"} alt={"flatIcon"}/>
                                 </div>
                                 <div className="info">
                                     <div className="content">
-                                        <h2>{user.name}</h2>
-                                        <div className='status-type'>
+                                        <div className='mid'>
+                                            <h2>{user.name}</h2>
+                                        </div>
+                                        {/* <div className='status-type'>
                                             <p><b>Tipo:</b> {user.userType == 'PATHFINDER' ? 'Desbravador' : user.userType == 'DIRECTION' ? 'Diretoria' : user.userType == 'EXECUTIVE' ? 'Executiva' : 'Eventual'}</p>
                                             <p><b>Status:</b> {user.active ? "Ativo" : "Inativo"}</p>
-                                        </div>
+                                        </div> */}
                                         <div className='btn-edit'>
                                             <img className="edit" src={"https://cdn-icons-png.flaticon.com/512/2280/2280532.png"} alt=""/>
                                         </div>
