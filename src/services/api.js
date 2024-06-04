ba505e90c81e581aeedd854884e2cca12ebe5bcd
Felipe Pinheiro <felipehyo@gmail.com>
2024-06-04 02:33:38 -0300

    Update backend to amazon ec2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://dbv-financeiro.herokuapp.com/pathfinders/'
+    // baseURL: 'https://dbv-financeiro.herokuapp.com/pathfinders/'
+    baseURL: 'http://18.229.136.190/pathfinders/'
     // baseURL: 'http://localhost:8080/pathfinders/'
     // baseURL: 'http://192.168.0.123:8080/pathfinders/'
 })
