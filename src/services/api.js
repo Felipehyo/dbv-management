34df45d6a4a7dd2f342d6b0d45546ec7e64c61ab
Felipe Pinheiro <felipehyo@gmail.com>
2024-06-11 21:02:08 -0300

    adjust

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://dbv-financeiro.herokuapp.com/pathfinders/'
-    baseURL: 'https://portaldbv.com/pathfinders/'
+    baseURL: 'https://dbv-financeiro.herokuapp.com/pathfinders/'
+    // baseURL: 'https://portaldbv.com/pathfinders/'
     // baseURL: 'http://localhost:8080/pathfinders/'
     // baseURL: 'http://192.168.0.113:8080/pathfinders/'
 })
