4871f5a5be85478e555ba16f7b12e3e31a7bb0b4
Felipe Pinheiro Dantas <felipehyo@gmail.com>
2025-07-11 01:57:13 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     // baseURL: 'https://portaldbv.com/pathfinders/'
     // baseURL: 'http://localhost:8080/pathfinders/'
-    baseURL: '18.231.17.148:8080/pathfinders/'
+    baseURL: 'http://18.231.17.148:8080/pathfinders/'
 })
 
 export default api;
