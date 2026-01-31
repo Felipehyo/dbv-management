e3c75c8196ebccf2ddd8c2b1a997e6c542b85be6
Felipe Pinheiro <felipehyo@gmail.com>
2026-01-31 15:59:28 -0300

    :bug: fix: Ajustando url do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://portaldbv.com/pathfinders/'
+    baseURL: 'https://portaldbv.com/pathfinders/v1/'
     // baseURL: 'http://localhost:8080/pathfinders/'
     // baseURL: 'http://localhost:8080/pathfinders/v1/'
     // baseURL: 'http://192.168.1.3:8080/pathfinders/v1/'
