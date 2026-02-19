ac3a37dcccad5cfcf93654e54bce3a9c3a954652
Felipe Pinheiro <felipehyo@gmail.com>
2026-02-19 20:48:09 -0300

    :recycle: fix: Ajuste de logo e título da aba do navegador

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import gbMenLogo from './assets/gb-men.png';
+
+const favicon = document.getElementById('app-favicon');
+if (favicon) {
+  favicon.setAttribute('href', gbMenLogo);
+  favicon.setAttribute('sizes', '32x32');
+}
+
+const appleTouchIcon = document.getElementById('app-apple-touch-icon');
+if (appleTouchIcon) {
+  appleTouchIcon.setAttribute('href', gbMenLogo);
+  appleTouchIcon.setAttribute('sizes', '180x180');
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
